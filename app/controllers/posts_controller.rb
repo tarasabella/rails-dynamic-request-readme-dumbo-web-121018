7e3f8ce1695bc267c5ed12cbb5a9a626c6c27cf5
Tara Sabella <tarasabella@gmail.com>
2019-01-14 15:49:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,16 +3,3 @@ class PostsController < ApplicationsController
   @posts = Post.find(params[:id])
 end 
 
-end 
-
-Def index 
-@pokemons = Pokemon.all
-Instance varriabke pokemon = all pokemon 
-Takes you to render 
-Render :index 
-#renders the index file 
-#index action shows everything about thsi resource 
-End 
-
-
-get "/post/:id, to: "posts#show"
